26f9abb99bb3fa8bb69155a0f7bb45d6831ff7df
Marie Vendran <marie.vendran@gmail.com>
2019-10-08 17:32:07 +0200

    correction

---
+++ src/main/java/com/wildcodeschool/Spring/Application.java
@@ -0,0 +1,51 @@
+package com.wildcodeschool.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/doctor/1")
+	@ResponseBody
+	public String wh() {
+		return "<a href='https://en.wikipedia.org/wiki/William_Hartnell'>William Hartnell</a>";
+	}
+
+	@RequestMapping("/doctor/2")
+	@ResponseBody
+	public String pt() {
+		return "<a href='https://en.wikipedia.org/wiki/Patrick_Troughton'>Patrick Troughton</a>";
+	}
+
+	@RequestMapping("/doctor/3")
+	@ResponseBody
+	public String jp() {
+		return "<a href='https://en.wikipedia.org/wiki/Jon_Pertwee'>Jon Pertwee</a>";
+	}
+
+	@RequestMapping("/doctor/4")
+	@ResponseBody
+	public String tb() {
+		return "<a href='https://en.wikipedia.org/wiki/Tom_Baker'>Tom Baker</a>";
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "<ul><li><a href='https://en.wikipedia.org/wiki/William_Hartnell'>William Hartnell</a></li>" +
+                "<li><a href='https://en.wikipedia.org/wiki/Patrick_Troughton'>Patrick Troughton</a></li>" +
+                "<li><a href='https://en.wikipedia.org/wiki/Jon_Pertwee'>Jon Pertwee</a></li>" +
+                "<li><a href='https://en.wikipedia.org/wiki/Tom_Baker'>Tom Baker</a></li></ul>";
+	}
+
+}
